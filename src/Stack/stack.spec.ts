cb0ab97348f021b34d5933b53292d7cefb04dbec
Andrew Park <atpark333@gmail.com>
2022-02-19 23:52:51 +0900

    test: add Stack tests

--- src/Stack/stack.spec.ts
+++ src/Stack/stack.spec.ts
@@ -1,7 +1,24 @@
 import Stack from '.';
 
 describe('Stack', () => {
+  let stack1: Stack;
+
+  beforeEach(() => {
+    stack1 = new Stack();
+  });
+
   it('should be defined', () => {
     expect(Stack).toBeDefined();
+  });
+
+  describe('pop', () => {
+    it('should be defined', () => {
+      expect(stack1.pop).toBeDefined();
+      expect(stack1.pop).toBeInstanceOf(Function);
+    });
+
+    it('should remove the "top" item', () => {
+
+    });
   });
 });
