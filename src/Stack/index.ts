9262028e8e6691e4745b992a789774fc308a5cd2
Andrew Park <atpark333@gmail.com>
2022-02-19 23:52:35 +0900

    feat: add a stack class that implements Collection

--- src/Stack/index.ts
+++ src/Stack/index.ts
@@ -1,6 +1,11 @@
-export default class Stack {
+import Collection from "@project/Collection";
+
+export default class Stack implements Collection {
   pop() {}
   push() {}
   peek() {}
   isEmpty() {}
+  toArray() {
+    return []
+  }
 }
